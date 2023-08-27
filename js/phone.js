0a58355c8e0f4d667aa337a039386821739cbd03
promorshed45 <138321084+promorshed45@users.noreply.github.com>
2023-08-28 01:00:58 +0600

    update

--- js/phone.js
+++ js/phone.js
@@ -10,14 +10,14 @@ const displayPhones = phones => {
     phoneContainer.textContent = '';
 
     const showAllContainer = document.getElementById('show-all-container');
-    if(phones.length > 5){
+    if(phones.length > 12){
         showAllContainer.classList.remove('hidden');
     }else{
         showAllContainer.classList.add('hidden');
     }
 
 
-    phones =phones.slice(0,5);
+    phones =phones.slice(0,12);
 
 
     phones.forEach(phone => {
